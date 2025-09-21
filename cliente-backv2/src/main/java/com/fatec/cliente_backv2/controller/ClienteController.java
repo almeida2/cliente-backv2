18d4d0fa47ae0de98e53c27e1859f31de09b491d
almeida1 <engsoftware1@gmail.com>
2025-09-21 12:01:39 -0300

    refatoracao - envelopamento da resposta api

--- cliente-backv2/src/main/java/com/fatec/cliente_backv2/controller/ClienteController.java
+++ cliente-backv2/src/main/java/com/fatec/cliente_backv2/controller/ClienteController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -27,7 +26,7 @@ public class ClienteController {
 
 	IClienteService clienteService;
 
-	@Autowired
+	//injecao da dependencia automatica pelo metodo construtor
 	public ClienteController(IClienteService clienteService) {
 		this.clienteService = clienteService;
 	}
@@ -41,10 +40,12 @@ public class ClienteController {
 		logger.info(">>>>>> apicontroller cadastro de cliente iniciado...");
 		
 		ClienteResponse c = clienteService.cadastrar(cliente);
-		if (!c.isSucesso()) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(c.getMensagem());
+		if (c.isSucesso()) {
+			ApiResponse<Cliente> response = new ApiResponse<>(c.getCliente(), "Cliente cadastrado com sucesso.");
+			return ResponseEntity.status(HttpStatus.CREATED).body(response);
 		} else {
-			return ResponseEntity.status(HttpStatus.CREATED).body(c.getCliente());
+			ApiResponse<Cliente> response = new ApiResponse<>(c.getMensagem());
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
 		}
 
 	}
