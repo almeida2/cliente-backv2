82e44d3b8068cb00eceb4f9ad371b1ff9558f176
almeida1 <engsoftware1@gmail.com>
2025-11-21 15:47:55 -0300

    refatoracao - doc interface IClienteService

--- cliente-backv2/src/main/java/com/fatec/cliente_backv2/service/IClienteService.java
+++ cliente-backv2/src/main/java/com/fatec/cliente_backv2/service/IClienteService.java
@@ -12,37 +12,30 @@ public interface IClienteService {
      * @return uma lista com todos os clientes registrados
      */
 	public List<Cliente> consultaTodos();
-	//Req09 Cadastrar Cliente
+	
 	/**
-     * Cadastra um novo cliente a partir dos dados informados.
-     *
+     * Req09 Cadastrar Cliente- Cadastra um novo cliente a partir dos dados informados.
      * @param cliente objeto ClienteDTO contendo os dados do cliente a ser cadastrado
      * @return o objeto Cliente cadastrado
      * @throws IllegalArgumentException se algum dado do cliente for inválido
      */
 	public Cliente cadastrar(ClienteDTO cliente);
-	//Req10 Consultar Cliente pelo CPF
 	 /**
-     * Consulta um cliente pelo CPF.
-     *
+     * Req10 Consultar Cliente pelo CPF - Consulta um cliente pelo CPF.
      * @param cpf o CPF do cliente a ser consultado
      * @return um Optional contendo o cliente encontrado, ou vazio se não existir
      */
 	public Optional<Cliente> consultarPorCpf(String cpf);
 	//Req11 Consulta agrupada pelo CEP (nao implementado)
-	//Req12 Atualizar informações do cliente 
 	/**
-     * Atualiza os dados de um cliente a partir do objeto ClienteDTO.
-     *
+     * Req12 Atualizar informações do cliente - Atualiza os dados de um cliente a partir do objeto ClienteDTO.
      * @param cliente objeto ClienteDTO contendo os dados atualizados do cliente
      * @return um Optional contendo o cliente atualizado, ou vazio se não existir
      * @throws IllegalArgumentException se os dados informados forem inválidos
      */
 	public Optional<Cliente> atualizar(ClienteDTO cliente);
-	//Req13 Exclusão pelo CPF
 	/**
-     * Exclui um cliente pelo CPF.
-     *
+     * Req13 Exclusão pelo CPF - Exclui um cliente pelo CPF.
      * @param cpf o CPF do cliente a ser excluído
      * @return true se o cliente foi excluído com sucesso, ou false se não encontrado
      */
